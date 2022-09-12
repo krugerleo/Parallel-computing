4e89810396fec7a4d1238d6a84855e395c58b02f
Leonardo Kruger <kruger.leob@gmail.com>
2022-09-12 16:18:59 -0300

    fix exec

--- aulaMPI/exec.sh
+++ aulaMPI/exec.sh
@@ -4,22 +4,22 @@ now="$(date +'%d-%m-%Y-%H-%M')"
 
 #!/bin/bash
 echo "Size (fileB,fileA,uniqAB);Serial Score;Serial LCS Time;Parallel Score;Parallel LCS Time;Execution" >> ./resultados/result$now.txt
-for x in `seq 30`
+for x in `seq 20`
 do
     mpirun -np 1 kruger entradas/file1.in entradas/file2.in entradas/uniqAB.in | tee -a ./resultados/result$now.txt >/dev/null
     echo "$x" >> ./resultados/result$now.txt
 done
-for y in `seq 30`
+for y in `seq 20`
 do
     mpirun -np 1 kruger entradas/fileC.in entradas/fileD.in entradas/uniqAB.in | tee -a ./resultados/result$now.txt >/dev/null
     echo "$y" >> ./resultados/result$now.txt
 done
-for z in `seq 30`
+for z in `seq 20`
 do
     mpirun -np 1 kruger entradas/fileA.in entradas/fileB.in entradas/uniqAB.in | tee -a ./resultados/result$now.txt >/dev/null
     echo "$z" >> ./resultados/result$now.txt
 done
-for z in `seq 30`
+for z in `seq 20`
 do
     mpirun -np 1 kruger entradas/file3.in entradas/file4.in entradas/uniqAB.in | tee -a ./resultados/result$now.txt >/dev/null
     echo "$z" >> ./resultados/result$now.txt
